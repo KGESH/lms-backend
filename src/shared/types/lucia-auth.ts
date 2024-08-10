b7a9bbe536ff1f84a5c32e0c31de88209490a1db
J222 <baram789456@gmail.com>
2024-08-11 00:26:51 +0900

    bump: SDK v0.1.21 프론트 Lucia auth 전용 db schema

--- src/shared/types/lucia-auth.ts
+++ src/shared/types/lucia-auth.ts
@@ -1,5 +1,9 @@
-import { users } from '../../infra/db/schema/user';
+import { users, userSessions } from '../../infra/db/schema/user';
 
 export const BackendUserDbTable = users;
 
 export type IBackendUserDbTable = typeof BackendUserDbTable;
+
+export const BackendUserSessionDbTable = userSessions;
+
+export type IBackendUserSessionDbTable = typeof BackendUserSessionDbTable;
