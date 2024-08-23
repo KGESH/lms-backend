a45983d8d35d4ec9c91639a2a2b1fa4e42021391
J222 <baram789456@gmail.com>
2024-08-23 12:49:18 +0900

    comment: 프론트엔드에서 사용할 db 테이블 타입 주석

--- src/shared/types/lucia-auth.ts
+++ src/shared/types/lucia-auth.ts
@@ -1,3 +1,10 @@
+/**
+ * 프론트엔드에서 사용할 User, UserSession 타입을 정의합니다.
+ * import 경로를 '상대 경로'로 설정해주세요.
+ * 옳은 경로 예시) import { User, UserSession } from '../../../sample/path';
+ * 아래와 같이 '절대 경로'로 설정하지 마세요.
+ * 잘못된 경로 예시) import { User, UserSession } from '@src/shared/types/lucia-auth';
+ */
 import { users, userSessions } from '../../infra/db/schema/user';
 
 export const BackendUserDbTable = users;
