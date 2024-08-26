17c054eeb24a6ba9077b4e078b85aa6c6c7ff896
J222 <baram789456@gmail.com>
2024-08-26 23:31:26 +0900

    refactor: course order DTO

--- src/v1/order/course/course-order.dto.ts
+++ src/v1/order/course/course-order.dto.ts
@@ -3,7 +3,7 @@ import { ISO8601, Price, Uuid } from '@src/shared/types/primitive';
 export type CourseOrderDto = {
   id: Uuid;
   userId: Uuid;
-  courseProductSnapshotId: Uuid;
+  productSnapshotId: Uuid;
   paymentMethod: string;
   amount: Price;
   paidAt: ISO8601 | null;
