833c0ea76102a53586c690447b48c67bd9d4caaf
J222 <baram789456@gmail.com>
2024-09-02 19:05:44 +0900

    chore: ping log

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { AppService } from '@src/app.service';
 import { SkipAuth } from '@src/core/decorators/skip-auth.decorator';
 import { SkipApiGuard } from '@src/core/decorators/skip-api-guard';
 
 @Controller()
 export class AppController {
+  private readonly logger = new Logger(AppController.name);
   constructor(private readonly appService: AppService) {}
 
   /**
@@ -17,6 +18,7 @@ export class AppController {
   @SkipAuth()
   @SkipApiGuard()
   async ping(): Promise<'pong'> {
+    this.logger.verbose('ping');
     return this.appService.ping();
   }
 }
