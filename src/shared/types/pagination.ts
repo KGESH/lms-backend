4ce14c67592a26da3f20221dd5d7f29d2e2e5452
J222 <baram789456@gmail.com>
2024-09-03 16:46:12 +0900

    feat: paginated result

--- src/shared/types/pagination.ts
+++ src/shared/types/pagination.ts
@@ -6,15 +6,37 @@ export type Pagination = {
    * default: 0
    */
   page: UInt;
+
   /**
    * item count.
    * default: 10
    */
   pageSize: UInt;
+
   /**
    * order by 'created_at'.
    * default: 'desc'.
    * Category API 호출시 정렬 기준이 ['created_at', 'desc']가 아닌 ['name', 'asc']로 설정됩니다.
    */
   orderBy: 'asc' | 'desc';
+};
+
+export type Paginated<TArray extends Array<unknown>> = {
+  /**
+   * paginated data.
+   * 데이터 조회 결과입니다.
+   */
+  data: TArray;
+
+  /**
+   * pagination info.
+   * 요청한 페이지네이션 정보입니다.
+   */
+  pagination: Pagination;
+
+  /**
+   * total count of database rows.
+   * 데이터베이스에 저장된 전체 데이터의 개수입니다.
+   */
+  totalCount: UInt;
 };
