b948687fc4a0934853b9478f7a109827b3d827a9
J222 <baram789456@gmail.com>
2024-09-06 15:09:23 +0900

    docs: post category access comment

--- src/v1/post/category/access/post-category-access.controller.ts
+++ src/v1/post/category/access/post-category-access.controller.ts
@@ -18,6 +18,7 @@ import { RolesGuard } from '@src/core/guards/roles.guard';
 import { AuthHeaders } from '@src/v1/auth/auth.headers';
 import { TypeGuardError } from 'typia';
 import { IErrorResponse } from '@src/shared/types/response';
+import { INVALID_LMS_SECRET } from '@src/core/error-code.constant';
 
 @Controller('v1/post-category/:categoryId/access')
 export class PostCategoryAccessController {
@@ -37,14 +38,24 @@ export class PostCategoryAccessController {
     status: 400,
     description: 'invalid request',
   })
+  @TypedException<IErrorResponse<401>>({
+    status: 401,
+    description: 'Session user error.',
+  })
   @TypedException<IErrorResponse<403>>({
     status: 403,
     description:
+      'Required role error. ' +
+      'or ' +
       'If [guest] role is included in roles, another role should not be included.',
   })
   @TypedException<IErrorResponse<409>>({
     status: 409,
     description: '[Category ID, Role] pair already exists.',
+  })
+  @TypedException<IErrorResponse<INVALID_LMS_SECRET>>({
+    status: INVALID_LMS_SECRET,
+    description: 'invalid LMS api secret',
   })
   @TypedRoute.Post('/')
   @Roles('admin', 'manager')
@@ -60,6 +71,34 @@ export class PostCategoryAccessController {
     });
   }
 
+  /**
+   * 게시판 카테고리 권한을 삭제합니다.
+   *
+   * 관리자 세션 id를 헤더에 담아서 요청합니다.
+   *
+   * @tag post-category
+   * @summary 게시판 카테고리 권한 생성 - Role('admin', 'manager')
+   */
+  @TypedException<TypeGuardError>({
+    status: 400,
+    description: 'invalid request',
+  })
+  @TypedException<IErrorResponse<401>>({
+    status: 401,
+    description: 'Session user error.',
+  })
+  @TypedException<IErrorResponse<403>>({
+    status: 403,
+    description: 'Required role error.',
+  })
+  @TypedException<IErrorResponse<404>>({
+    status: 404,
+    description: 'Delete target not found.',
+  })
+  @TypedException<IErrorResponse<INVALID_LMS_SECRET>>({
+    status: INVALID_LMS_SECRET,
+    description: 'invalid LMS api secret',
+  })
   @TypedRoute.Delete('/')
   @Roles('admin', 'manager')
   @UseGuards(RolesGuard)
