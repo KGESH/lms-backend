485d6b3b0980cf2ba9d4a6c55e77735c84899fce
J222 <baram789456@gmail.com>
2024-09-25 21:40:25 +0900

    fix: file helper import path

--- test/e2e/helpers/db/lms/file.helper.ts
+++ test/e2e/helpers/db/lms/file.helper.ts
@@ -1,6 +1,6 @@
-import { IFile, IFileCreate } from '@src/v1/file/file.interface';
-import { TransactionClient } from '@src/infra/db/drizzle.types';
-import { dbSchema } from '@src/infra/db/schema';
+import { IFile, IFileCreate } from '../../../../../src/v1/file/file.interface';
+import { TransactionClient } from '../../../../../src/infra/db/drizzle.types';
+import { dbSchema } from '../../../../../src/infra/db/schema';
 
 export const createManyFiles = async (
   params: IFileCreate[],
